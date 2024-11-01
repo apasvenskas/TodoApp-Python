a5205f66a160c0f2970e87bbc099c075b94f6980
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-10-31 22:23:12 -0700

    initial commit

---
+++ TodoApp/main.py
@@ -0,0 +1,9 @@
+promptMessage = "Enter todo:"
+todo1 = input(promptMessage)
+todo2 = input(promptMessage)
+todo3 = input(promptMessage)
+
+todos = [todo1, todo2, todo3, "Hello"]
+print(todos)
+
+print(type(todos))
