f2b153e2d27550ffb9bdd3c75cd004e96fc1d318
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-11-02 15:01:34 -0700

    added a while loop

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,9 +1,9 @@
 promptMessage = "Enter todo:"
-todo1 = input(promptMessage)
-todo2 = input(promptMessage)
-todo3 = input(promptMessage)
 
-todos = [todo1, todo2, todo3, "Hello"]
-print(todos)
+todos = []
 
-print(type(todos))
+while True:
+    todo = input(promptMessage)
+    print(todo.capitalize())
+    todos.append(todo)
+    
