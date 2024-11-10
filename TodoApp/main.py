113d9e4c1f96431140f09a0698cb2b9911b489c4
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-11-10 14:44:50 -0800

    added a text file and code for storing todos

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,3 @@
-todos = []
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -6,8 +5,16 @@ while True:
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n" # \n is a break in line for todos.txt
+
+            file = open('todos.txt', 'r') # read the todos.txt file
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+            file = open('todos.txt', 'w') # writes in todos.txt file
+            file.writelines(todos)
+            file.close()
         case 'show' | 'display':
             for index, item in enumerate(todos):
                 row = f"{index + 1}. {item}"
