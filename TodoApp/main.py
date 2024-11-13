fac61fc8225d013e858fad7a3252a6b9f9ae9b19
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-11-12 20:08:41 -0800

    fixed erros with show case and complete case

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,38 +1,45 @@
-
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
+
         case 'add':
             todo = input("Enter a todo: ") + "\n" # \n is a break in line for todos.txt
+            with open('todos.txt', 'a') as file: # Use 'a' to append to the file
+                file.write(todo)
 
-            file = open('todos.txt', 'r') # read the todos.txt file
-            todos = file.readlines()
-            file.close()
-
-            todos.append(todo)
-            file = open('todos.txt', 'w') # writes in todos.txt file
-            file.writelines(todos)
-            file.close()
         case 'show' | 'display':
-            file = open('todos.txt', 'r')
-            todos = file.readline()
-            file.close()
+            with open('todos.txt', 'r') as file: 
+                todos = file.readlines() 
+                new_todos = [item.strip('\n') for item in todos] 
+                
+                for index, item in enumerate(new_todos): 
+                    row = f"{index + 1}. {item}" 
+                    print(row)
 
-            for index, item in enumerate(todos):
-                row = f"{index + 1}. {item}"
-                print(row)
         case 'edit':
-            number = int(input("Number of the todo to edit: "))
-            number = number - 1
-            new_todo = input("Enter new todo: ")
-            todos[number] = new_todo# Acces the todos index at number value. 
+            with open('todos.txt', 'r') as file: 
+                todos = file.readlines() 
+                number = int(input("Number of the todo to edit: ")) - 1 
+                new_todo = input("Enter new todo: ") + "\n" 
+                
+                todos[number] = new_todo 
+                with open('todos.txt', 'w') as file: 
+                    file.writelines(todos)
+
         case 'complete':
-            number = int(input("Number of the todo to complete: "))
-            todos.pop(number - 1)
+            with open('todos.txt', 'r') as file: 
+                todos = file.readlines() 
+
+            number = int(input("Number of the todo to complete: ")) - 1 
+            todos.pop(number) 
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'exit':
             break
+
         case _:
             print("You entered a wrong command")
 
