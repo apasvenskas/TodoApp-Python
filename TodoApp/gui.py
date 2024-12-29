095c2994016824711c93d82aba90196c40b6d08a
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-12-29 15:17:25 -0800

    initial GUI setup

--- TodoApp/gui.py
+++ TodoApp/gui.py
@@ -1,3 +1,10 @@
 import functions
 import FreeSimpleGUI
 
+label = FreeSimpleGUI.Text("Type in a todo")
+input_box = FreeSimpleGUI.InputText(tooltip="Enter todo")
+add_button = FreeSimpleGUI.Button("Add")
+
+window = FreeSimpleGUI.Window('My Todo App', layout=[[label], [input_box, add_button]])
+window.read() # Displays the window on the screen. 
+window.close()
