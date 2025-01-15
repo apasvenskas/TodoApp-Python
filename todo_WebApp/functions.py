248d41fe74679dffaa6778ea40d2c66f16d49b86
Audrius Pasvenskas <apasvenskas@outlook.com>
2025-01-15 15:05:39 -0800

    initial setup

---
+++ todo_WebApp/functions.py
@@ -0,0 +1,16 @@
+def get_todos(filepath="todos.txt"): # file path a general path witch can be specified in the call later such as todos = get_todos("todos.txt")
+    """
+        Read a text file and return the list of to do items.
+    """
+    with open(filepath, 'r') as file: # Use 'a' to append to the file
+        todos = file.readlines()
+    return todos
+
+def write_todos(todos_arg, filepath="todos.txt"):
+     with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+if __name__ == "main":
+    print("Hello")
+    print(get_todos())
+# print(help(get_todos))
